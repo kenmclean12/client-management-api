ead3c35f243fa54d1f5c0e99379592af456b8b25
kenmclean12 <kenmclean20@gmail.com>
2025-12-05 16:52:43 -0800

    initial commit

---
+++ api1/Program.cs
@@ -0,0 +1,21 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/", () =>
+{
+    Console.WriteLine("First App");
+    return "Hello World!";
+});
+
+app.Run();
