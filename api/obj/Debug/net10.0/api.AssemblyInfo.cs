dccba2ca9f1cadb07f92af7ba2a44792e1696824
kenmclean12 <kenmclean20@gmail.com>
2025-12-14 16:25:08 -0800

    1

--- api/obj/Debug/net10.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net10.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48d372d25bd0dcba6784c10bdff2b08e38f165f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a38b02b0637dbf401782ce7040ccab5b0e84cd7")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
